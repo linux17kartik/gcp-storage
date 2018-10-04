9bc9d247347a40f65b81658e6b47a02429ceccea
kartik <34565796+linux17kartik@users.noreply.github.com>
2018-10-04 17:10:49 +0530

    static variable

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-read -p "Enter No. of database want to create:" database
-read -p "Enter No. of tables want to create in databases:" tables
+# read -p "Enter No. of database want to create:" database
+# read -p "Enter No. of tables want to create in databases:" tables
+database=10 
+tables=10
+
 for (( x=1 ; x<=$database ; x++ ))
 do 
 echo "create database mysql"$x";" >> mysql.sql
@@ -9,3 +12,18 @@ do
 echo "create table mysql"$x".table"$y"(name varchar(255));" >> mysql.sql
 done
 done
+
+
+
+
+# #!/bin/bash
+# read -p "Enter No. of database want to create:" database
+# read -p "Enter No. of tables want to create in databases:" tables
+# for (( x=1 ; x<=$database ; x++ ))
+# do 
+# echo "create database mysql"$x";" >> mysql.sql
+# for (( y=1 ; y<=$tables ; y++ ))
+# do
+# echo "create table mysql"$x".table"$y"(name varchar(255));" >> mysql.sql
+# done
+# done
